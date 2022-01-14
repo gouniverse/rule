ded33580c0b68572b23b359ac9a76a0992755b97
Milan Lesichkov <lesichkovm@gmail.com>
2022-01-14 19:46:20 +0000

    Created business rule type

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+)
+
+type BusinessRule struct {
+	Context interface{}
+}
+
+func (r BusinessRule) Condition() bool {
+	return true
+}
+
+// Evaluate evaulates the condition and returns the result
+func (r BusinessRule) Evaluate() bool {
+	return true
+}
+
+// Passes - returns true if the rule evaluates to true
+func (r BusinessRule) Passes() bool {
+	return r.Evaluate()
+}
+
+// Fails returns true if the rule evaluates to false
+func (r BusinessRule) Fails() bool {
+	return !r.Evaluate()
+}
 
 func main() {
-	fmt.Printf("Hello, world")
+	rule := BusinessRule{}
+	log.Println(rule.Passes())
 }
