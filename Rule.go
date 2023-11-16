54148b9d08a397616f7b07ac439261704039f627
Milan Lesichkov <lesichkovm@gmail.com>
2023-11-16 21:10:51 +0000

    Added fail and pass messages

--- Rule.go
+++ Rule.go
@@ -6,6 +6,14 @@ type Rule struct {
 
 	// Condition function
 	condition func(context any) bool
+
+	// Optional extras
+
+	// helper array to hold fail messages
+	failMessages []string
+
+	// helper array to hold pass messages
+	passMessages []string
 }
 
 var _ RuleInterface = (*Rule)(nil) // verify it extends the RuleInterface interface
@@ -41,4 +49,56 @@ func (rule *Rule) Passes() bool {
 // Evaluates the condition and returns the result
 func (rule *Rule) Evaluate() bool {
 	return rule.condition(rule.context)
+}
+
+// AddFailMessage adds a fail message
+func (rule *Rule) AddFailMessage(message string) {
+	rule.failMessages = append(rule.failMessages, message)
+}
+
+// AddPassMessage adds a pass message
+func (rule *Rule) AddPassMessage(message string) {
+	rule.passMessages = append(rule.passMessages, message)
+}
+
+// FailMessages retrieves the fail messages
+func (rule *Rule) FailMessages() []string {
+	return rule.failMessages
+}
+
+// PassMessages retrieves the pass messages
+func (rule *Rule) PassMessages() []string {
+	return rule.passMessages
+}
+
+// FailMessageFirst retrieves the first fail message
+func (rule *Rule) FailMessageFirst() string {
+	if len(rule.failMessages) < 1 {
+		return ""
+	}
+	return rule.failMessages[0]
+}
+
+// FailMessageLast retrieves the last fail message
+func (rule *Rule) FailMessageLast() string {
+	if len(rule.failMessages) < 1 {
+		return ""
+	}
+	return rule.failMessages[len(rule.failMessages)-1]
+}
+
+// PassMessageFirst retrieves the first pass message
+func (rule *Rule) PassMessageFirst() string {
+	if len(rule.passMessages) < 1 {
+		return ""
+	}
+	return rule.passMessages[0]
+}
+
+// PassMessageLast retrieves the last pass message
+func (rule *Rule) PassMessageLast() string {
+	if len(rule.passMessages) < 1 {
+		return ""
+	}
+	return rule.passMessages[len(rule.passMessages)-1]
 }
